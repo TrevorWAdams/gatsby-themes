b9ca0751819ccb43dec060eaa464a948a8dbf682
Trevor Adams <trevorwadams@outlook.com>
2019-06-25 17:25:41 -0600

    task: bye for today

--- sites/legacy-project/gatsby-config.js
+++ sites/legacy-project/gatsby-config.js
@@ -1,19 +1,31 @@
-const remarkPlugins = [require('remark-slug')]
+const remarkPlugins = [require("remark-slug")]
 
 module.exports = {
-  __experimentalThemes: ['gatsby-theme-ui'],
+  __experimentalThemes: ["gatsby-theme-ui"],
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-catch-links',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-catch-links",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: 'gatsby-plugin-mdx',
+      resolve: "gatsby-plugin-mdx",
       options: {
-        extensions: ['.mdx', '.md'],
+        extensions: [".mdx", ".md"],
         remarkPlugins,
-        gatsbyRemarkPlugins: ['gatsby-remark-prismjs', { resolve: 'gatsby-remark-images' }],
-      },
-    },
-  ],
+        gatsbyRemarkPlugins: [
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-prismjs",
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590
+            }
+          }
+        ]
+      }
+    }
+  ]
 }
